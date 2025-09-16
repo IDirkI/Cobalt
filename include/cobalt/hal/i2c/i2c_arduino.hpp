80c7746b8ca9508bf89630dae7e12bbbb5d8a9e3
Berk <79381262+IDirkI@users.noreply.github.com>
2025-09-16 01:13:51 -0400

    Add i2c-arduino implementation

--- include/cobalt/hal/i2c/i2c_arduino.hpp
+++ include/cobalt/hal/i2c/i2c_arduino.hpp
@@ -4,12 +4,15 @@
 
 #include "i2c_base.hpp"
 
+#if defined ARDUINO
+#include <Wire.h>
+
 namespace cobalt::hal {
 
 constexpr uint8_t I2C_ARDUINO_DEFAULT_SDA = 4; 
 constexpr uint8_t I2C_ARDUINO_DEFAULT_SCL = 5; 
 
-constexpr uint8_t I2C_ARDUINO_DEFAULT_PORT = 0; 
+constexpr uint8_t I2C_ARDUINO_DEFAULT_BUS = 0; 
 constexpr I2CMode I2C_ARDUINO_DEFAULT_MODE = I2CMode::Master;
 constexpr uint32_t I2C_ARDUINO_DEFAULT_FREQUENCY = 100000;
 
@@ -22,6 +25,7 @@ class I2CArduino : public I2CBase {
     private:
         uint8_t pinSDA_;
         uint8_t pinSCL_;
+        TwoWire *wire_ = nullptr;
 
     public:
         // ---------------- Constructors ----------------
@@ -37,11 +41,13 @@ class I2CArduino : public I2CBase {
         // ----------------  Arduino I2C Member Functions ----------------
 
         bool init(uint8_t address, uint8_t bus, I2CMode mode, uint32_t frequency) override;
-        bool write(uint8_t address, uint8_t *wBuff, size_t len = 1) override;
-        bool read(uint8_t address, uint8_t *rBuff, size_t len = 1) override;
-        bool writeReg(uint8_t address, uint8_t regAddr, uint8_t *wBuff, size_t len = 1) override;
-        bool readReg(uint8_t address, uint8_t regAddr, uint8_t *rBuff, size_t len = 1) override;
-        bool writeRead(uint8_t address, uint8_t *wBuff, uint8_t *rBuff, size_t readLen) override;
+        bool write(uint8_t address, const uint8_t *wBuff, size_t len) override;
+        bool read(uint8_t address, uint8_t *rBuff, size_t len) override;
+        bool writeRead(uint8_t address, const uint8_t *wBuff, size_t writeLen, uint8_t *rBuff, size_t readLen) override;
+        bool writeReg(uint8_t address, uint8_t regAddr, uint8_t wBuff) override;
+        bool readReg(uint8_t address, uint8_t regAddr, uint8_t *rBuff) override;
 };
 
 } // cobalt::hal
+
+#endif
--- include/cobalt/hal/i2c/i2c_base.hpp
+++ include/cobalt/hal/i2c/i2c_base.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stddef.h>
 #include <stdint.h>
 
 namespace cobalt::hal {
@@ -19,11 +20,11 @@ class I2CBase {
         virtual ~I2CBase() = default;
 
         virtual bool init(uint8_t address, uint8_t bus, I2CMode mode, uint32_t frequency) = 0;
-        virtual bool write(uint8_t address, uint8_t *wBuff, size_t len) = 0;
+        virtual bool write(uint8_t address, const uint8_t *wBuff, size_t len) = 0;
         virtual bool read(uint8_t address, uint8_t *rBuff, size_t len) = 0;
-        virtual bool writeReg(uint8_t address, uint8_t regAddr, uint8_t *wBuff, size_t len) = 0;
-        virtual bool readReg(uint8_t address, uint8_t regAddr, uint8_t *rBuff, size_t len) = 0;
-        virtual bool writeRead(uint8_t address, uint8_t *wBuff, uint8_t *rBuff, size_t readLen) = 0;
+        virtual bool writeRead(uint8_t address, const uint8_t *wBuff, size_t writeLen, uint8_t *rBuff, size_t readLen) = 0;
+        virtual bool writeReg(uint8_t address, uint8_t regAddr, uint8_t wBuff) = 0;
+        virtual bool readReg(uint8_t address, uint8_t regAddr, uint8_t *rBuff) = 0;
 };
 
 } // cobalt::hal
