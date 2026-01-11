1112609fc0080e6b2857952e80cfcee56694b724
berk <berkyilmaz2005@gmail.com>
2026-01-11 09:50:34 -0500

    Restart Joint structure

---
+++ include/cobalt/kinematics/config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+namespace cobalt::kinematics {
+    // ---------------- Types ----------------
+    /**
+     *  @brief Default alias for indexing
+     */
+    using id_t  = std::uint8_t;
+
+} // cobalt::kinematics
